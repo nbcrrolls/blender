34e2cd30efb39233bad56757c3e1ed02be3d734e
nadyawilliams <nadya@sdsc.edu>
2015-05-07 18:14:39 -0700

    use blender verison from a single include file

--- src/blender/version.mk
+++ src/blender/version.mk
@@ -1,7 +1,15 @@
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ..
+VERSION.MK.INCLUDE = blender.version.mk
+include $(VERSION.MK.INCLUDE)
+
 PKGROOT	    = /opt/blender
 NAME        = blender
 EXTENDED    = linux-glibc211
-VERSION     = 2.74
+VERSION     = $(BLENDERVER)
+
+SOURCE_PKG = $(NAME)-$(VERSION)-$(EXTENDED)-$(ARCH)
+
 RELEASE 	= 0
 TARBALL_POSTFIX	= tar.bz2
 
---
+++ src/version.mk
@@ -0,0 +1,1 @@
+BLENDERVER = 2.74

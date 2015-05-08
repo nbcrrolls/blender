509066aa0d3e4b53a01d346c9a0b7693e5c2708f
nadyawilliams <nadya@sdsc.edu>
2015-05-07 19:13:21 -0700

    add rendering webservice, initial

---
+++ src/blender-opal/version.mk
@@ -0,0 +1,10 @@
+
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ..
+VERSION.MK.INCLUDE = blender.version.mk
+include $(VERSION.MK.INCLUDE)
+
+NAME    = blender-opal
+VERSION = 1.0
+RELEASE = 0
+
